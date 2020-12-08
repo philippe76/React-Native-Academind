a029912fce346f84ac4b0aafc0703685993117e3
Philippe Lanougadere <soufiane92230@gmail.com>
2020-12-08 16:47:44 +0100

    Begin Academind Course

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import uuid from 'react-native-uuid'
 
 export default function App() {
+
+  const [inputText, setInputText] = useState('');
+  const [tasks, setTasks] = useState([])
+
+  const inputHandler = (text) => {
+    setInputText(text)
+  }
+
+  const addHandler = () => {
+    setTasks(prevTasks => [...prevTasks, inputText]);
+    setInputText('')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 50}}>
+      <View style={styles.container}>
+        <TextInput 
+          placeholder="Things To Do ..." 
+          style={styles.input} 
+          onChangeText={inputHandler}
+          value={inputText}
+        />
+        <Button title="ADD" color="#b0c4de" onPress={addHandler}/>
+      </View>
+      <View>
+        {tasks.map(item => <Text key={uuid.v4()}>{item}</Text>)}
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
+  input: {
+    borderColor: 'darkgrey',
+    borderWidth: 2,
+    borderRadius: 5,
+    padding: 8,
+    width: '80%'    
+  }
 });
+
