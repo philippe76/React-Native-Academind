3bf033f8f65a9def898bd768fdf3c3048c7f5151
Philippe Lanougadere <soufiane92230@gmail.com>
2020-12-10 18:18:30 +0100

    Add delete button to each new item

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import {Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Button} from 'react-native';
 
 const ListItem = props => {
     return (
-        <TouchableOpacity activeOpacity={0.8} style={styles.listItem} onPress={props.deleteTask}>
-            <Text>{props.title}</Text>
-        </TouchableOpacity>
+        <View style={styles.container}>
+            <TouchableOpacity activeOpacity={0.8} style={styles.listItem} >
+                <Text>{props.title}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={props.deleteTask} style={styles.button}>
+                <Text>X</Text>
+            </TouchableOpacity>     
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: 'center'
+    },
     listItem: {
         padding: 10,
         marginVertical: 10,
@@ -18,6 +27,20 @@ const styles = StyleSheet.create({
         borderColor: "darkgrey",
         borderWidth: 2,
         borderRadius: 5,
+        width: '75%'
+    },
+    button: {
+        padding: 10,
+        marginVertical: 10,
+        marginLeft: 10,
+        backgroundColor: '#ea6969',
+        borderColor: "#928C8C",
+        borderColor: "darkgrey",
+        borderWidth: 2,
+        borderRadius: 5,
+        width: '21%',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
 
